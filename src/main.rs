01d27ae32eb55ecbca75239714d4b0790ad5c6d4
Élise <elise@eiss.fr>
2024-03-15 11:47:12 +0100

    sort in seperate func

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,28 @@ fn main() {
         walkgeneric_test(&arg);
     }
 }
+
+type CustomState = (usize, usize);
+type CustomDirEnt = jwalk::DirEntry<CustomState>;
+
+// TODO: sort folders first?
+fn sort_predicate(
+    lhs: &jwalk::Result<CustomDirEnt>,
+    rhs: &jwalk::Result<CustomDirEnt>,
+) -> std::cmp::Ordering {
+    match (lhs, rhs) {
+        (Ok(a), Ok(b)) => a.file_name.cmp(&b.file_name),
+        (Ok(_), Err(_)) => Ordering::Less,
+        (Err(_), Ok(_)) => Ordering::Greater,
+        (Err(_), Err(_)) => Ordering::Equal,
+    }
+}
+
 fn walkgeneric_test(dir: &str) {
-    let walk_dir = jwalk::WalkDirGeneric::<(usize, bool)>::new(dir)
+    let walk_dir = jwalk::WalkDirGeneric::<CustomState>::new(dir)
         .process_read_dir(|_depth, _path, read_dir_state, children| {
             // 1. Custom sort
-            // TODO: sort folders first?
-            children.sort_by(|a, b| match (a, b) {
-                (Ok(a), Ok(b)) => a.file_name.cmp(&b.file_name),
-                (Ok(_), Err(_)) => Ordering::Less,
-                (Err(_), Ok(_)) => Ordering::Greater,
-                (Err(_), Err(_)) => Ordering::Equal,
-            });
+            children.sort_by(sort_predicate);
             // 2. Custom filter
             // children.retain(|dir_entry_result| {
             //     dir_entry_result
@@ -40,19 +51,21 @@ fn walkgeneric_test(dir: &str) {
             // 4. Custom state
             // dbg!(*read_dir_state);
             *read_dir_state += 1;
-            children.first_mut().map(|dir_entry_result| {
+            let a = *read_dir_state;
+            children.iter_mut().for_each(|dir_entry_result| {
                 if let Ok(dir_entry) = dir_entry_result {
-                    dir_entry.client_state = true;
+                    dir_entry.client_state = a;
                 }
             });
         })
-        .root_read_dir_state(5);
+        .root_read_dir_state(0);
 
     for entry in walk_dir {
         if let Ok(entry) = entry {
             println!(
-                "{}{}",
-                if entry.client_state { "first " } else { "" },
+                "{} {}",
+                //if entry.client_state { "first " } else { "" },
+                entry.client_state,
                 entry.path().display(),
             );
         }
