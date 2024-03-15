5ece92b6ebe23cf82896c997b0984c2d92a6c682
Élise <elise@eiss.fr>
2024-03-15 16:29:11 +0100

    stop recursion when a file indicating a project is found

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, ffi::OsStr, usize};
 
 fn main() {
     for arg in std::env::args().skip(1) {
@@ -6,7 +6,7 @@ fn main() {
     }
 }
 
-type CustomState = (usize, usize);
+type CustomState = (usize, bool);
 type CustomDirEnt = jwalk::DirEntry<CustomState>;
 
 // TODO: sort folders first?
@@ -22,50 +22,83 @@ fn sort_predicate(
     }
 }
 
-fn walkgeneric_test(dir: &str) {
-    let walk_dir = jwalk::WalkDirGeneric::<CustomState>::new(dir)
-        .process_read_dir(|_depth, _path, read_dir_state, children| {
-            // 1. Custom sort
-            children.sort_by(sort_predicate);
-            // 2. Custom filter
-            // children.retain(|dir_entry_result| {
-            //     dir_entry_result
-            //         .as_ref()
-            //         .map(|dir_entry| {
-            //             dir_entry
-            //                 .file_name
-            //                 .to_str()
-            //                 .map(|s| !s.starts_with('.'))
-            //                 .unwrap_or(false)
-            //         })
-            //         .unwrap_or(false)
-            // });
-            // 3. Custom skip
-            children.iter_mut().for_each(|dir_entry_result| {
-                if let Ok(dir_entry) = dir_entry_result {
-                    if dir_entry.depth == 2 {
-                        dir_entry.read_children_path = None;
-                    }
-                }
-            });
-            // 4. Custom state
-            // dbg!(*read_dir_state);
-            *read_dir_state += 1;
-            let a = *read_dir_state;
-            children.iter_mut().for_each(|dir_entry_result| {
-                if let Ok(dir_entry) = dir_entry_result {
-                    dir_entry.client_state = a;
-                }
-            });
-        })
-        .root_read_dir_state(0);
+const PROJ_CONFIG_FILESS: &'static [&'static str] = &["package.json", "cargo.toml", "Makefile"];
 
+fn is_project_config_file(filename: &std::ffi::OsStr) -> bool {
+    if let Ok(filename) = filename.try_into() {
+        PROJ_CONFIG_FILESS.contains(&filename)
+    } else {
+        false
+    }
+}
+
+#[cfg(test)]
+mod test_is_project_config_file {
+    #[test]
+    fn case_a() {
+        let os_filename = std::ffi::OsString::from("package.json");
+        assert_eq!(super::is_project_config_file(&os_filename), true);
+    }
+}
+
+fn walkgeneric_test(dir: &str) {
+    let walk_dir = jwalk::WalkDirGeneric::<CustomState>::new(dir).process_read_dir(
+        |_depth, _path, _read_dir_state, children| {
+            // INFO: base usage for this callback
+            // https://docs.rs/jwalk/0.8.1/jwalk/index.html#extended-example
+            children.sort_by(sort_predicate);
+
+            let mut count: usize = 0;
+            children
+                .iter_mut()
+                .filter_map(|entry| match entry {
+                    Ok(entry) if is_project_config_file(entry.file_name()) => {
+                        count += 1;
+                        Some(entry)
+                    }
+                    _ => None,
+                })
+                .for_each(|entry| entry.client_state = true);
+            // TODO: set read_children_path to none for all children here if
+            // we found a project file
+            if count != 0 {
+                children
+                    .iter_mut()
+                    .filter_map(|entry| match entry {
+                        Ok(entry) if entry.read_children_path != None => Some(entry),
+                        _ => None,
+                    })
+                    .for_each(|entry| entry.read_children_path = None);
+            }
+
+            // children.iter_mut().for_each(|dir_entry_result| {
+            //     if let Ok(dir_entry) = dir_entry_result {
+            //         if is_project_config_file(dir_entry.file_name()) {
+            //             dir_entry.client_state = true;
+            //         }
+            //     }
+            // });
+        },
+    );
+
+    walk_dir
+        .into_iter()
+        .filter_map(|item| match item {
+            Ok(item) if item.client_state => Some(item),
+            _ => None,
+        })
+        .for_each(|item| println!("{}", item.path().display()));
+    return;
     for entry in walk_dir {
         if let Ok(entry) = entry {
             println!(
                 "{} {}",
-                //if entry.client_state { "first " } else { "" },
-                entry.client_state,
+                if entry.client_state {
+                    "project file "
+                } else {
+                    ""
+                },
+                //entry.client_state,
                 entry.path().display(),
             );
         }
