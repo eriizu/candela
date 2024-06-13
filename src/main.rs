b4cf01808defb655737f61d447bcaa95cff83759
Élise <elise@eiss.fr>
2024-06-13 15:23:48 +0200

    main on top

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,20 @@ mod recursive_cleaner;
 
 use clap::Parser;
 
+fn main() {
+    let opt = Cli::parse();
+    match opt.command {
+        Commands::Clean { mut base_dirs } => {
+            recursive_cleaner::RecursiveCleaner::new().run(base_dirs.drain(..));
+        }
+        Commands::Flatten(flatten_opt) => flattener::run(flatten_opt),
+        Commands::Version => {
+            print!("{CANDLE_ASCII_ART}\n\n");
+            println!("candela v{}\nA personnal qol helper.", VERSION);
+        }
+    }
+}
+
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 // INFO: ASCII candle copyright of Joan Stack
 const CANDLE_ASCII_ART: &'static str = r#"
@@ -36,18 +50,4 @@ enum Commands {
     },
     Flatten(flattener::Cli),
     Version,
-}
-
-fn main() {
-    let opt = Cli::parse();
-    match opt.command {
-        Commands::Clean { mut base_dirs } => {
-            recursive_cleaner::RecursiveCleaner::new().run(base_dirs.drain(..));
-        }
-        Commands::Flatten(flatten_opt) => flattener::run(flatten_opt),
-        Commands::Version => {
-            print!("{CANDLE_ASCII_ART}\n\n");
-            println!("candela v{}\nA personnal qol helper.", VERSION);
-        }
-    }
 }
